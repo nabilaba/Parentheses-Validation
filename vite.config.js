3fc2543f3fa7bc0e07237e9fd04ef63af6ca43b6
nabilaba <nabilazizbimaanggita@gmail.com>
2022-07-07 22:23:58 +0700

    feat: update vite config js and homepage package.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  base: "./",
+  plugins: [react()],
+  resolve: {
+    alias: {
+      src: resolve(__dirname, "src"),
+    },
+  },
+  build: {
+    outDir: "build",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+    chunkSizeWarningLimit: 1600,
+  },
+});
